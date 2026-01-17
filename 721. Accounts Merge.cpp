33e8c5b0b7afad9739dbfc2dd5ff69d9fa2358d4
Sandipan Paria <sandipanparia6@gmail.com>
2026-01-17 15:20:05 +0530

    Update 721. Accounts Merge.cpp

--- 721. Accounts Merge.cpp
+++ 721. Accounts Merge.cpp
@@ -1,1 +1,94 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+class DisjointSet {
+   // vector<int> rank, parent, size;
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n=accounts.size();
+        DisjointSet ds(n);
+        unordered_map<string,int>mppMailNode;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+                string mail=accounts[i][j];
+                //doesnot appears else to end
+                if(mppMailNode.find(mail)==mppMailNode.end()){
+                    mppMailNode[mail]=i;
+                }
+                else{
+                    ds.unionBySize(i,mppMailNode[mail]);
+                }
+            }
+        }
+        vector<vector<string>>mergeStrng(n);
+            for(auto it:mppMailNode){
+                string mail=it.first;
+                int node=ds.findUPar(it.second);
+                mergeStrng[node].push_back(mail);
+
+            }
+        
+        vector<vector<string>>ans;
+         for(int i=0;i<n;i++){
+            if(mergeStrng[i].size()==0)continue;
+            sort(mergeStrng[i].begin(), mergeStrng[i].end());
+            vector<string>temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it:mergeStrng[i]){
+                temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+
+    }
+};
