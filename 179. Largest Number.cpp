b9a6df8b6c7ae1311ea1dc3a712eaa2bbbe02c1e
Sandipan Paria <sandipanparia6@gmail.com>
2026-03-30 01:44:20 +0530

    Implement largestNumber function to form largest number

---
+++ 179. Largest Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static bool cmp(int a, int b) {
+        string x = to_string(a);
+        string y = to_string(b);
+        return x + y > y + x;
+    }
+    string largestNumber(vector<int>& nums) {
+       sort(nums.begin(), nums.end(), cmp);
+
+        if (nums[0] == 0) return "0";  
+
+        string s = "";
+        for (int num : nums) {
+            s += to_string(num);
+        }
+        return s;
+    }
+};
