9113e37717d972680279a2753e6a37ac8b20e7ec
Sandipan Paria <sandipanparia6@gmail.com>
2026-08-13 03:58:51 +0530

    Create  Optimal Partition of String.cpp

---
+++ Optimal Partition of String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        unordered_map<char,int>mp;
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]]>0){
+                cnt++;
+                mp.clear();
+                mp[s[i]]++;
+            }
+            else{
+                mp[s[i]]++;
+            }
+        }
+        return cnt+1;
+    }
+};
